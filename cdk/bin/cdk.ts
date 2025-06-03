5016115961ab3e4f7bbca297395aeae35f5829a3
Shubham Singh <development.shubhamsingh@gmail.com>
2025-06-04 02:06:20 +0530

    Update the permission set for the cdk deployment

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -6,16 +6,8 @@ import 'source-map-support/register'
 // Import CDK core library
 import * as cdk from 'aws-cdk-lib'
 
-// Import stack definitions
-import { VpcStack } from '../lib/vpc-stack'
-import { DatabaseStack } from '../lib/database-stack'
-import { RedisStack } from '../lib/redis-stack'
-import { EcsStack } from '../lib/ecs-stack'
-import { ApiStack } from '../lib/api-stack'
-import { FrontendStack } from '../lib/frontend-stack'
-import { MediaStack } from '../lib/media-stack'
-import { CloudWatchStack } from '../lib/cloudwatch-stack'
-import { S3Stack } from '../lib/s3-stack'
+// Import main stack
+import { MainStack } from '../lib/main-stack'
 
 // DotEnv Module - Only load in local environment
 if (process.env.NODE_ENV === 'local') {
@@ -45,7 +37,7 @@ const COMMON_TAGS = {
     ManagedBy: 'CDK'
 }
 
-// For each region create the stacks
+// For each region create the stack
 regions.forEach(region => {
     
     // Define base stack name
@@ -54,99 +46,11 @@ regions.forEach(region => {
     // Get account ID
     const accountId = process.env.AWS_ACCOUNT_ID
 
-    // Create VPC stack
-    // const vpc_stack = new VpcStack(app, `${baseStackName}-vpc-${region}`, {
-    //     env: {
-    //         account: accountId,
-    //         region: region
-    //     },
-    //     tags: {
-    //         ...COMMON_TAGS,
-    //         Region: region
-    //     }
-    // })
+    // Create stack name with region suffix
+    const stackName = `${baseStackName}-${region}`
 
-    // Create Database stack
-    // const database_stack = new DatabaseStack(app, `${baseStackName}-database-${region}`, {
-    //     vpc: vpc_stack.vpc,
-    //     env: {
-    //         account: accountId,
-    //         region: region
-    //     },
-    //     tags: {
-    //         ...COMMON_TAGS,
-    //         Region: region
-    //     }
-    // })
-
-    // Create Redis stack
-    // const redis_stack = new RedisStack(app, `${baseStackName}-redis-${region}`, {
-    //     vpc: vpc_stack.vpc,
-    //     env: {
-    //         account: accountId,
-    //         region: region
-    //     },
-    //     tags: {
-    //         ...COMMON_TAGS,
-    //         Region: region
-    //     }
-    // })
-
-    // Create ECS stack
-    // const ecs_stack = new EcsStack(app, `${baseStackName}-ecs-${region}`, {
-    //     vpc: vpc_stack.vpc,
-    //     env: {
-    //         account: accountId,
-    //         region: region
-    //     },
-    //     tags: {
-    //         ...COMMON_TAGS,
-    //         Region: region
-    //     }
-    // })
-
-    // Create API stack
-    // const api_stack = new ApiStack(app, `${baseStackName}-api-${region}`, {
-    //     vpc: vpc_stack.vpc,
-    //     domain_name: process.env.DOMAIN_NAME,
-    //     env: {
-    //         account: accountId,
-    //         region: region
-    //     },
-    //     tags: {
-    //         ...COMMON_TAGS,
-    //         Region: region
-    //     }
-    // })
-
-    // Create Frontend stack
-    // const frontend_stack = new FrontendStack(app, `${baseStackName}-frontend-${region}`, {
-    //     domain_name: process.env.DOMAIN_NAME,
-    //     env: {
-    //         account: accountId,
-    //         region: region
-    //     },
-    //     tags: {
-    //         ...COMMON_TAGS,
-    //         Region: region
-    //     }
-    // })
-
-    // Create Media stack
-    // const media_stack = new MediaStack(app, `${baseStackName}-media-${region}`, {
-    //     domain_name: process.env.DOMAIN_NAME,
-    //     env: {
-    //         account: accountId,
-    //         region: region
-    //     },
-    //     tags: {
-    //         ...COMMON_TAGS,
-    //         Region: region
-    //     }
-    // })
-
-    // Create S3 stack
-    const s3_stack = new S3Stack(app, `${baseStackName}-s3-${region}`, {
+    // Create main stack
+    new MainStack(app, stackName, {
         env: {
             account: accountId,
             region: region
@@ -156,25 +60,4 @@ regions.forEach(region => {
             Region: region
         }
     })
-
-    // Create CloudWatch stack
-    const cloudwatch_stack = new CloudWatchStack(app, `${baseStackName}-cloudwatch-${region}`, {
-        env: {
-            account: accountId,
-            region: region
-        },
-        tags: {
-            ...COMMON_TAGS,
-            Region: region
-        }
-    })
-
-    // Add stack dependencies
-    // database_stack.addDependency(vpc_stack)
-    // redis_stack.addDependency(vpc_stack)
-    // ecs_stack.addDependency(vpc_stack)
-    // api_stack.addDependency(vpc_stack)
-    // api_stack.addDependency(ecs_stack)
-    // frontend_stack.addDependency(api_stack)
-    // media_stack.addDependency(api_stack)
 }) 
