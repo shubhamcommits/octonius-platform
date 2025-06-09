b1e81e7aefc662483c1a65f0c20463ae3b354dfb
Shubham Singh <development.shubhamsingh@gmail.com>
2025-06-10 01:39:22 +0530

    feat: add web infrastructure module for S3 and CloudFront

---
+++ terraform/modules/web/variables.tf
@@ -0,0 +1,20 @@
+variable "environment" {
+  description = "Environment name (e.g., dev, prod)"
+  type        = string
+}
+
+variable "project_name" {
+  description = "Project name"
+  type        = string
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "common_tags" {
+  description = "Common tags to apply to all resources"
+  type        = map(string)
+  default     = {}
+} 
