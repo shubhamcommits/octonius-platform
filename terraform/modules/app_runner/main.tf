a267438438ad8fa51abca93218dea77635da6d4b
Shubham Singh <development.shubhamsingh@gmail.com>
2025-06-12 12:07:16 +0530

    Refactor: Deployment Pipelines

--- terraform/modules/app_runner/main.tf
+++ terraform/modules/app_runner/main.tf
@@ -62,7 +62,7 @@ resource "aws_apprunner_service" "main" {
 
 # VPC Connector
 resource "aws_apprunner_vpc_connector" "main" {
-  vpc_connector_name = "${var.environment}-${var.project_name}-vpc-connector"
+  vpc_connector_name = "${var.environment}-${var.project_name}-${var.region}-vpc-connector"
   subnets            = var.subnet_ids
   security_groups    = [var.app_runner_security_group_id]
 }
