c3e8b8e0de81016cac3fc613f7631d4320e0683f
Shubham Singh <development.shubhamsingh@gmail.com>
2025-06-14 03:59:39 +0530

    feat(auth): update login component with improved UI and functionality

--- services/octonius-web/src/app/modules/auth/login/login.component.ts
+++ services/octonius-web/src/app/modules/auth/login/login.component.ts
@@ -1,11 +1,47 @@
 import { Component } from '@angular/core'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { ReactiveFormsModule } from '@angular/forms'
+import { CommonModule } from '@angular/common'
+import { AuthService } from '../../shared/services/auth.service'
+import { Router } from '@angular/router'
+import { environment } from '../../../../environments/environment'
+import { finalize } from 'rxjs/operators'
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm: FormGroup
+  isLoading = false;
 
+  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]]
+    })
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      this.isLoading = true;
+      const email = this.loginForm.get('email')?.value
+      this.authService.requestOtp(email)
+      .pipe(
+        finalize(() => this.isLoading = false)
+      )
+      .subscribe({
+        next: (response: any) => {
+          this.isLoading = false
+          this.router.navigate(['/auths/verify-otp'], { state: { email, is_new_user: response.data.exists } });
+        },
+        error: (error: any) => {
+          this.isLoading = false
+          console.error('Error fetching user:', error)
+        }
+      })
+    }
+  }
 }
