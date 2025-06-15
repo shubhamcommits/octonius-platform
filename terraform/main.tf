3b11503cf37a516799159741f9d3d5853eb83b6d
Shubham Singh <development.shubhamsingh@gmail.com>
2025-06-15 15:29:13 +0530

    Refactor: Add VPC connector with public gateway

--- terraform/main.tf
+++ terraform/main.tf
@@ -99,8 +99,8 @@ module "vpc" {
   enable_dns_hostnames = true
   enable_dns_support   = true
 
-  enable_nat_gateway = false
-  single_nat_gateway = false
+  enable_nat_gateway = true
+  single_nat_gateway = true
 
   tags = local.common_tags
 }
@@ -196,7 +196,7 @@ module "app_runner" {
   project_name                 = local.project_name
   region                       = local.aws_region
   vpc_id                       = module.vpc.vpc_id
-  subnet_ids                   = module.vpc.public_subnet_ids
+  subnet_ids                   = module.vpc.private_subnet_ids
   secret_name_pattern          = "${var.environment}-${local.project_name}-platform-service-env-${local.aws_region}"
   app_runner_security_group_id = aws_security_group.app_runner.id
 
