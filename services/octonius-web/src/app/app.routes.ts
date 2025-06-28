1e7428c98e4d95ffe7aa36dbf081ddb5ed1b8051
Shubham Singh <development.shubhamsingh@gmail.com>
2025-06-28 16:13:31 +0530

    Add routing module and updates for octonius web

--- services/octonius-web/src/app/app.routes.ts
+++ services/octonius-web/src/app/app.routes.ts
@@ -1,28 +1,26 @@
 import { Routes } from '@angular/router'
 import { AuthGuard } from './modules/shared/services/auth.guard'
-import { NonAuthGuard } from './modules/shared/services/non-auth.guard'
+import { WorkplaceGuard } from './modules/shared/services/workplace.guard'
 
 export const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    redirectTo: 'myspace',
-    canActivate: [AuthGuard]
+    redirectTo: 'myspace'
   },
   {
     path: 'auths',
-    loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule),
-    canActivate: [NonAuthGuard]
+    loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule)
   },
   {
     path: 'myspace',
     loadChildren: () => import('./modules/my-space/my-space.module').then(m => m.MySpaceModule),
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard, WorkplaceGuard]
   },
   {
     path: 'workplace',
     loadChildren: () => import('./modules/workplace/workplace.module').then(m => m.WorkplaceModule),
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard, WorkplaceGuard]
   },
   {
     path: '**',
